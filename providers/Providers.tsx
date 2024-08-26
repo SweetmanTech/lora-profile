443a317af8b28452114db739a3ebe49b695af0be
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-26 20:29:25 -0300

    I fix build errors.

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,18 +1,9 @@
 'use client'
 
-import { ZoraCreateProvider } from './ZoraCreateProvider'
-import { PaymasterProvider } from './PaymasterProvider'
-import { PointsProvider } from './PointsProvider'
 import WagmiProvider from './WagmiProvider'
 
 const Providers = ({ children }: { children: React.ReactNode }) => (
-  <WagmiProvider>
-    <PaymasterProvider>
-      <PointsProvider>
-        <ZoraCreateProvider>{children}</ZoraCreateProvider>
-      </PointsProvider>
-    </PaymasterProvider>
-  </WagmiProvider>
+  <WagmiProvider>{children}</WagmiProvider>
 )
 
 export default Providers
