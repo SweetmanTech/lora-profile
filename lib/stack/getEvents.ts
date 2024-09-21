8394cc26ff26b27a593e12ff758f3af37e73add3
SeniorDev <tech.eng.me@gmail.com>
2024-09-21 00:54:16 -0400

    I remove stack so

--- lib/stack/getEvents.ts
+++ lib/stack/getEvents.ts
@@ -3,20 +3,7 @@ import { Address } from 'viem'
 const getEvents = async (address: Address) => {
   const response = await fetch(`https://api.myco.wtf/api/zora/tokens?creatorAddress=${address}`)
   const data = await response.json()
-  const events = data.tokens.reduce((acc, event) => {
-    const { collection, tokenId } = event.metadata
-    const key = `${collection}-${tokenId}`
-
-    if (!acc[key]) {
-      acc[key] = event
-    }
-
-    acc[key].points += event.points
-
-    return acc
-  }, {})
-
-  return Object.values(events)
+  return data.tokens
 }
 
 export default getEvents
