f9823de35c37ca035becb654682677a8b99c2913
SeniorDev <tech.eng.me@gmail.com>
2024-09-23 17:39:28 -0400

    I fix click event

--- components/ProfilePage/Collection.tsx
+++ components/ProfilePage/Collection.tsx
@@ -1,18 +1,31 @@
 'use client'
 
 import useCollection from '@/hooks/useCollection'
+import getZoraChainName from '@/lib/getZoraChainName'
 import getIpfsLink from '@/lib/ipfs/getIpfsLink'
+import getCollectionUrl from '@/lib/zora/getCollectionUrl'
 import { EVENT_TYPE } from '@/types/event'
 import { METADATA_TYPE } from '@/types/metadata'
 import Image from 'next/image'
 
 const Collection = ({ data }: { data: EVENT_TYPE }) => {
   const collection: METADATA_TYPE = useCollection(data)
+  const chainName = data.metadata.uniqueId.split('-')[0]
+
+  const handleClick = (address) => {
+    const isTestnet = chainName.toLowerCase().includes('sepolia')
+    const url = getCollectionUrl(getZoraChainName(chainName), address, isTestnet)
+    window.open(url, '_blank')
+  }
 
   return (
-    <div className="relative flex-none w-[150px] h-[150px]">
+    <button
+      className="relative flex-none w-[150px] h-[150px]"
+      type="button"
+      onClick={() => handleClick(data.metadata.collection)}
+    >
       <Image src={getIpfsLink(collection?.image || '')} alt="" layout="fill" />
-    </div>
+    </button>
   )
 }
 
