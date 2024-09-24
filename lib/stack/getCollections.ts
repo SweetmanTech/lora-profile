561ea39475a9ae952c6747b93e484427657f8706
SeniorDev <tech.eng.me@gmail.com>
2024-09-24 10:12:01 -0400

    I fix feedback

--- lib/stack/getCollections.ts
+++ lib/stack/getCollections.ts
@@ -3,12 +3,12 @@ import { Address } from 'viem'
 const getCollections = async (address: Address) => {
   const response = await fetch(`https://api.myco.wtf/api/zora/collections?creator=${address}`)
   const data = await response.json()
-  const collections = data.collections
+  const collections = data.collections.reverse()
   const tokens = []
 
   for (const collection of collections) {
     const tokensCreated = parseInt(collection.tokensCreated, 10)
-    for (let tokenId = 1; tokenId <= tokensCreated; tokenId++) {
+    for (let tokenId = tokensCreated; tokenId >= 1; tokenId--) {
       tokens.push({
         collectionAddress: collection.address,
         tokenId,
