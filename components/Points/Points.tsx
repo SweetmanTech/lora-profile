35466df38298e22ce907b7adf260d89c0747b717
SeniorDev <tech.eng.me@gmail.com>
2024-10-08 15:14:15 -0400

    fix: max height.

--- components/Points/Points.tsx
+++ components/Points/Points.tsx
@@ -6,7 +6,7 @@ const Points = () => {
   const { metadataOfTokens, loading } = useCollectionProvider()
 
   return (
-    <div className="w-screen md:w-full grid grid-cols-3 gap-1 md:gap-2 overflow-y-auto max-h-[270px]">
+    <div className="w-screen md:w-full grid grid-cols-3 gap-1 md:gap-2 overflow-y-auto max-h-[270px] md:max-h-[400px]">
       {!loading && metadataOfTokens.map((metadata, index) => <Point key={index} data={metadata} />)}
     </div>
   )
